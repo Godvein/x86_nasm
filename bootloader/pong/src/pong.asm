01946b17bfa424aa497e04802357b7f1b356d3df
ANkit Dware <sabindware@gmail.com>
2025-01-22 14:59:59 +0545

    ball size

--- bootloader/pong/src/pong.asm
+++ bootloader/pong/src/pong.asm
@@ -9,12 +9,44 @@ start:
 	mov bl, 0x00 ;background color black
 	int 0x10 ;interupt to set background
 	
+	call draw_ball
+
+draw_ball:
+	mov di, 0 ;x offset
+	mov si, 0 ;y offset
+	
+draw_ball_x:
+	
 	mov ah, 0x0c ;draw pixel function
 	mov al, 0x0f ;color whilte
 	mov bh, 0 ;page number
-	mov cx, 0x0a ; x position 
-	mov dx, 0x0a ; y position
+
+	mov cx, [ball_x] ; x position 
+	mov dx, [ball_y] ; y position	
+	
+	add cx, di ;add x offset
+	add dx, si ;add y offset
+	
 	int 0x10 ;interrupt to draw pixel
+	
+	cmp di, [ball_size] ;compare x to ball size 
+	inc di ;increase x offset
+	jb draw_ball_x ;jump if less than or equal to 0
+
+draw_ball_y:
+	mov di, 0 ;reset x offset
+	inc si ;increase y offset
+	cmp si, [ball_size] ;compare y to ball size
+	jb draw_ball_x ;jump if less than or equal to 0
+	ret 
+	
+	
+ball_x:
+	db 0x0a
+ball_y:
+	db 0x0a
+ball_size:
+	db 0x05
 
 times 510-($-$$) db 0
 db 0x55,0xaa
